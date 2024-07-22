cbb3659a98b9ad07f5ea8e15f08f0f0db7091881
Noah Dos Santos <noahpds@proton.me>
2024-07-21 17:04:10 -0700

    feat: add commit linting

---
+++ commitlint.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [2, 'always', 72],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'style',
+        'test'
+      ]
+    ],
+    'subject-empty': [2, 'never'],
+    'subject-case': [0]
+  }
+};
