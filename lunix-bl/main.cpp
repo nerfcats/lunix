2518fe55a60f855d6106d8b4bf8d2f06a1d0a2e5
Noah Dos Santos <noahpds@proton.me>
2024-07-29 15:28:30 -0700

    chore(bootloader): remove redundant screen clear command

--- lunix-bl/main.cpp
+++ lunix-bl/main.cpp
@@ -86,7 +86,6 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    system("clear");
     std::cout << BOLD_WHITE << "LUNIX BOOTLOADER (" << bMode << ")" << RESET << "\n\n";
 
     if (runPostBuild) {
