19b579f7c4edc50c18a202206cba3255ba2fec4b
Noah Dos Santos <noahpds@proton.me>
2024-07-29 16:42:22 -0700

    fix(build-date-set): fix file not found error

--- lunix-sys/build-date-set.sh
+++ lunix-sys/build-date-set.sh
@@ -3,5 +3,6 @@
 # Get the current date and time
 build_date=$(date +"%Y-%m-%d %H:%M:%S")
 
+touch ../lunix-bl/build/.builddate
 # Write the build date to .builddate file
 echo "$build_date" > ../lunix-bl/build/.builddate
