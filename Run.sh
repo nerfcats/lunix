b47aaeb2f4d952235470c275e948eef11debf12b
Noah Dos Santos <chocobrowndoggo@gmail.com>
2024-09-29 17:53:25 -0700

    feat(run): run bootloader with -b

--- Run.sh
+++ Run.sh
@@ -4,7 +4,7 @@ cd lunix-bl/build
 
 if [ -e "lunix-bl" ]; then
   # To bypass hash checks run with --ignore-hash-err
-  ./lunix-bl # Run the bootloader
+  ./lunix-bl -b # Run the bootloader
 else
   echo "The bootloader doesn't exist. Have you built Lunix properly? Were there any build errors?"
 fi
